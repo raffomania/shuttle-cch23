25aecf3eb728153979a5e0b340e7503af7ed260b
Rafael Epplée <hello@rafa.ee>
2023-12-01 13:43:31 +0100

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{http::StatusCode, routing::get, Router};
+use anyhow::{anyhow, Context};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +15,50 @@ async fn fake_error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
+async fn day1(Path(nums): Path<String>) -> Result<String, AppError> {
+    let num_list: Result<Vec<i64>, _> = nums
+        .split("/")
+        .map(|s| s.parse::<i64>())
+        .collect::<std::result::Result<Vec<_>, _>>();
+    Ok(num_list?
+        .into_iter()
+        .reduce(|a, b| a ^ b)
+        .ok_or(anyhow!("Failed to XOR :("))?
+        .pow(3)
+        .to_string())
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(fake_error));
+        .route("/-1/error", get(fake_error))
+        .route("/1/*nums", get(day1));
 
     Ok(router.into())
+}
+
+// Make our own error that wraps `anyhow::Error`.
+struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
